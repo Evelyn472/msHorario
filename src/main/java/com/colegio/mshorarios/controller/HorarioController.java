56c4edaad75a5e09f5401b88e3e01f2f0cd36f6c
Evelyn <eve.mora@duocuc.cl>
2026-05-17 20:46:55 -0400

    Refactor: Se agrega versión v1 a la URL del controlador de horarios

--- src/main/java/com/colegio/mshorarios/controller/HorarioController.java
+++ src/main/java/com/colegio/mshorarios/controller/HorarioController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/horarios")
+@RequestMapping("/api/v1/horario")
 @RequiredArgsConstructor
 public class HorarioController {
 
